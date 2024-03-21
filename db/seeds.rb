4fb6d7926b5fe0e85cd8e1d317d751c24ec89204
petre-enciu <petre202001@yahoo.com>
2024-03-21 10:52:01 +0200

    prepare for render

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Seeding..."
+Category.create(name:"Shoes", description: "Sneakers, derbies, etc")
+Category.create(name:"Trausers", description: "Every pants you could imagine")
+Category.create(name:"Shirts", description: "Long and short sleeves shirts")
+Category.create(name:"Teeshirts", description: "Designer teeshirts")
+Category.create(name:"Suits", description: "Designer suits")
+
+Product.create(name:"Sneakers", description:"Nike",price:"1000", category_id:1, active:true)
+Product.create(name:"Sneakers", description:"Adidas",price:"100", category_id:1, active:true)
+Product.create(name:"Short pants", description:"Levi's",price:"150", category_id:2, active:true)
+Product.create(name:"Jeans", description:"Levi's",price:"200", category_id:2, active:true)
+
+
+Admin.create(email:"admin@example.com", password:"password")
